98129f174b85abdac72189fe809e9ae1e393966c
Komdog <komdogart@gmail.com>
2021-06-17 22:47:07 -0400

    Added quiz command

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ async function init_commands(){
 
     const data = YAML.parse(commands) // Get Commands from YAML
 
+    await console.log('-- Loading Commands')
     data.map(command => console.log(`Loaded slash command: ${command.name}`))
     const command = await client.guilds.cache.get(guildId)?.commands.set(data);
 
     const perms = [{
-            id: '850653065011003412',
+            id: '850662019559981076',
             permissions:[
                 {
                     id: '789252395015733248',
@@ -40,6 +41,7 @@ async function init_commands(){
 
     }]
 
+    await console.log('-- Setting Command Permission')
     await client.guilds.cache.get(guildId)?.commands.setPermissions(perms);
 
 }
@@ -48,13 +50,20 @@ async function init_commands(){
 
 client.on('interaction', async interaction =>{
 
+    // console.log(interaction.commandID)
+
     if(!interaction.isCommand()) return;
-    if(interaction.channel.id != "850630909761421342") return interaction.reply(`Bot command don't work in the channel. Please go to <#850630909761421342>`, {ephemeral: true});
+    if(interaction.channel.id != "820093352862416926" && interaction.user.id != "125687298485518336") return interaction.reply(`Bot command don't work in the channel. Please go to <#820093352862416926>`, {ephemeral: true});
     if(interaction.commandName == 'ask') return cmd.askQuestion(client, interaction, '850631317950562304', "Question Sent")
-    if(interaction.commandName == 'achievement') return cmd.achievement(client, interaction)
+    if(interaction.commandName == 'achievements') return cmd.achievements(client, interaction)
+    if(interaction.commandName == 'giveachievement') return cmd.giveachievement(client, interaction)
     if(interaction.commandName == 'beatbanger') return cmd.beatBanger(client, interaction)
     if(interaction.commandName == 'serverinfo') return cmd.serverInfo(client, interaction)
     if(interaction.commandName == 'profile') return cmd.profile(client, interaction)
+    if(interaction.commandName == 'report') return cmd.report(client, interaction)
+    if(interaction.commandName == 'quiz') return cmd.quiz(client, interaction)
+
+    
 
 })
  
--- modules/commands.js
+++ modules/commands.js
@@ -3,6 +3,9 @@ const Func = require('./functions')
 const axios = require('axios').default;
 const moment = require('moment')
 
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('./ach.json');
+
 exports.profile = async (client, interaction)=>{
 
     var user_param = interaction.options.get('user')
@@ -21,24 +24,46 @@ exports.profile = async (client, interaction)=>{
     `)
     .setColor(member.displayHexColor)
     .setThumbnail(user.avatarURL())
+    .setFooter(`requested by #${interaction.user.tag}`)
     .setTimestamp()
     await interaction.reply(embed)
 
-    
-
 }
 
-exports.achievement = async (client, interaction)=>{
-
-    console.log(interaction.options)
+exports.giveachievement = async (client, interaction)=>{
 
     var user = interaction.options.get('user').user
     var ach = interaction.options.get('achievement').value
 
+    let ach_arr = db.get(`${user.id}`) ?? []
+    ach_arr.push(`${ach}`)
+
+    db.set(`${user.id}`, ach_arr);
+    db.sync();
+
     let embed = new Discord.MessageEmbed()
-    .setAuthor(`${interaction.guild.name} Server Information`, interaction.guild.iconURL())
-    .setDescription(`Gave ${user} the ✨ **${ach}** ✨ achievement!`)
+    .setAuthor(`Achievement Get!`, user.avatarURL())
+    .setDescription(`${user} got the ✨ **${ach}** ✨ achievement!`)
     .setTimestamp()
+
+    await interaction.reply(embed)
+
+}
+
+exports.achievements = async (client, interaction)=>{
+
+    var user_param = interaction.options.get('user')
+
+    user = user_param != null ? user_param.user : interaction.user
+    member = user_param != null ? user_param.member : interaction.member
+
+    let achievement_array = db.get(`${user.id}`) ?? ["Achievementless"]
+
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(`${user.username}'s Achievements`, user.avatarURL())
+    .setDescription(`${achievement_array.map(ach => `✨ **${ach}** ✨`).join("\n")}`)
+    .setTimestamp()
+    .setFooter(`requested by #${interaction.user.tag}`)
     await interaction.reply(embed)
 }
 
@@ -77,15 +102,84 @@ exports.askQuestion = async (client, interaction, channelID, reply)=>{
 
 }
 
+exports.report = async (client, interaction) =>{
+
+    let report_array = interaction.options.map(o => o.value)
+   
+    let title = `Report from ${interaction.user.tag}`
+    let color = '#00f'
+    let description = report_array[1]
+    let footer = `reported in #${interaction.channel.name}`
+    
+    let embed = new Discord.MessageEmbed()
+    .setTitle(title)
+    .setColor(color)
+    .addField('**Reported User**', `<@${report_array[0]}>`)
+    .addField('**Reason:**', description)
+    .setFooter(footer)
+   
+    let mod_guild = client.guilds.resolve('843703074996486184')
+    mod_guild.channels.resolve('852798849087963156').send(embed)
+    await interaction.reply(`Reported user <@${report_array[0]}>. Thanks for submitting!`, {ephemeral: true})
+
+}
+
+exports.quiz = async (client, interaction)=>{
+
+    var head = {headers:{'User-Agent': 'BunBot/1.0 (by Komdog on e621)'}}
+
+    let max_images = 50
+    let tags = [
+        'order:favcount',
+        'type:png'
+    ]
+    axios.get(`https://e621.net/posts.json?limit=${max_images}&tags=${tags.join(" ")}`,head)
+    .then(res=>{
+        let e621 = res.data.posts[Math.floor(Math.random()*max_images)]
+        let answer = e621.tags.artist
+
+        if (answer.includes('conditional_dnp')){
+            answer.splice(answer.indexOf('conditional_dnp'), 1)
+        }
+
+        if (answer.includes('sound_warning')){
+            answer.splice(answer.indexOf('sound_warning'), 1)
+        }
+        
+        if (answer.includes('avoid_posting')){
+            answer.splice(answer.indexOf('avoid_posting'), 1)
+        }
+        
+
+        console.log(`The artist is ${answer}`)
+        let embed = new Discord.MessageEmbed()
+        .setTitle('e621 Artist Quiz')
+        .setDescription('Who drew this picture? You have 10 seconds 🕐')
+        .setImage(e621.sample.url)
+        interaction.reply(embed)
+        let filter = message => {return message.content.toLowerCase() == e621.tags.artist}    
+        interaction.channel.awaitMessages(filter, {max:1, time:10000, errors:['time']})
+        .then(collected =>{
+            let array = collected.map(items => {return items})
+            interaction.channel.send(`✨${array[0].author} Got it first! The answer was ${answer}✨`)
+            console.log("You answered correctly")
+        })
+        .catch(err =>{
+            interaction.channel.send(`Times Up! The artists was **${answer}**`)
+        })
+    })
+
+}
+
 exports.beatBanger = async (client, interaction)=>{
 
     interaction.options.map(async choice =>{
 
         if(choice.value == "version")
         {
-            axios.get('https://pastebin.com/raw/FdQg2yfM')
+            axios.get('https://firestore.googleapis.com/v1/projects/bunfan-db/databases/(default)/documents/beat-banger/info')
             .then(async res =>{
-                let version = (res.data.version)
+                let version = res.data.fields.version.stringValue
                 let embed = new Discord.MessageEmbed()
                 .setDescription(`Beat Banger is currently on version \`${version}\``)
                 await interaction.reply(embed)
